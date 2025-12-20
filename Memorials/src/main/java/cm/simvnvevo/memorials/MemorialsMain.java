274252f9247964e951cedb6cd35b4322fb7d85a6
luffy126 <simon.ledezma2005@gmail.com>
2025-12-20 02:39:10 -0300

    ahora si

---
+++ Memorials/src/main/java/cm/simvnvevo/memorials/MemorialsMain.java
@@ -0,0 +1,17 @@
+package cm.simvnvevo.memorials;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MemorialsMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getServer().getConsoleSender().sendMessage("Activando plugin Memorials...");
+    }
+
+    @Override
+    public void onDisable(){
+        Bukkit.getServer().getConsoleSender().sendMessage("Apagando Memorials...");
+    }
+}
